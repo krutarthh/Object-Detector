7bc9a53c7c59c552cd4d84e1bf13a9590ac55950
Krutarth Patel <93881557+krutarthh@users.noreply.github.com>
2022-08-10 15:46:07 -0400

    Add files via upload

---
+++ src/utilities.js
@@ -0,0 +1,15 @@
+export const drawRect=(detections,ctx)=>{
+    detections.forEach(prediction => {
+        const[x,y,width,height]=prediction['bbox'];
+        const text = prediction['class'];
+        const color ='red'
+        ctx.strokeStyle=color
+        ctx.font='19px Arial'
+        ctx.fillStyle=color
+        ctx.beginPath()
+        ctx.fillText(text,x,y)
+        ctx.rect(x,y,width,height)
+        ctx.stroke()
+        
+    });
+}
